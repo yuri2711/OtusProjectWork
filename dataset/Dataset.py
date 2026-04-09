46482973bf4ab01f94b7cdbc04c917570109d5bf
Tarasenko <79991621127@yandex.ru>
2026-04-09 11:40:50 +0300

    local commit

--- dataset/Dataset.py
+++ dataset/Dataset.py
@@ -15,7 +15,7 @@ def __init__():
     else:
         print('Initialization failed')
 
-def create_dataset(symbol: str, window_len: int = 30 , predict_len: int = 3):
+def create_dataset(symbol: str, window_len: int = 30, predict_len: int = 10):
     """
     Для вызова текущей функции будет проверка на инициализацию и подключение к терминалу.
     Далее идет выгрузка данных из терминала Metatrader5
@@ -35,7 +35,7 @@ def create_dataset(symbol: str, window_len: int = 30 , predict_len: int = 3):
         __init__()
     point = 0.00001
 
-    df = pd.DataFrame(mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M5, 0, 99000))
+    df = pd.DataFrame(mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_H1, 0, 99000))
     df.drop(columns=['tick_volume', 'real_volume'], inplace=True)
     df['time'] = pd.to_datetime(df['time'], unit='s', utc=True)
     df.set_index('time', inplace=True)
@@ -46,18 +46,19 @@ def create_dataset(symbol: str, window_len: int = 30 , predict_len: int = 3):
 
 
     X, y = [], []
-    for i in range(len(scaled_data) - window_len):
+    for i in range(len(scaled_data) - window_len - predict_len):
         X.append(scaled_data[i:i+window_len])
-        past_close = df['close'][i+window_len - 1]
+        past_close = df['close'].iloc[i+window_len - 1]
         tmp = -1
+        data_index = df.index[i+window_len - 1]
         for _y in range(i + window_len, i + window_len + predict_len):
-            low_diff = (past_close - df['close'][_y]) / point
-            high_diff = (df['close'][_y] - past_close) / point
+            low_diff = (past_close - df['close'].iloc[_y]) / point
+            high_diff = (df['close'].iloc[_y] - past_close) / point
 
-            if high_diff > 100:
+            if high_diff > 200:
                 tmp = 0
                 break
-            elif low_diff > 100:
+            elif low_diff > 200:
                 tmp = 1
                 break
         y.append(tmp)
@@ -66,7 +67,36 @@ def create_dataset(symbol: str, window_len: int = 30 , predict_len: int = 3):
     y = np.array(y)
 
 
+    # indexes = np.where(y == -1)[0].tolist()
+
+    # X_TWO = np.delete(X, indexes, axis=0)
+    # y_TWO = np.delete(y, indexes)
+
+    mask = y == 0
+    y[mask] = 1
+
+    mask2 = y == -1
+    y[mask2] = 0
+    #
+    nonzero = np.count_nonzero(y == 0)
+    count_nonzero = np.count_nonzero(y > 0)
+    print(f'nonzero: {nonzero}, count_nonzero: {count_nonzero}')
+
+    nonzero_count_nonzero = abs(nonzero - count_nonzero)
+    if count_nonzero > nonzero:
+        _index = np.where(y > 0)[0].tolist()
+        indices_to_remove = _index[:nonzero_count_nonzero]
+
+        # Удаляем элементы по этим индексам
+        y = np.delete(y, indices_to_remove)
+        X = np.delete(X, indices_to_remove, axis=0)
+        nonzero = np.count_nonzero(y == 0)
+        count_nonzero = np.count_nonzero(y > 0)
+        print(f'nonzero: {nonzero}, count_nonzero: {count_nonzero}')
+
     split = int(0.8 * len(X))
+    # split_TWO = int(0.8 * len(X_TWO))
+
     X_train, X_test = X[:split], X[split:]
     y_train, y_test = y[:split], y[split:]
 
@@ -78,7 +108,19 @@ def create_dataset(symbol: str, window_len: int = 30 , predict_len: int = 3):
     X_test_tensor = torch.tensor(X_test, dtype=torch.float32).unsqueeze(1)
     y_test_tensor = torch.tensor(y_test, dtype=torch.long)
 
+    # X_train_TWO, X_test_TWO = X_TWO[:split_TWO], X_TWO[split_TWO:]
+    # y_train_TWO, y_test_TWO = y_TWO[:split_TWO], y_TWO[split_TWO:]
+    #
+    #
+    # # Преобразование в тензоры PyTorch
+    # X_train_tensor_TWO = torch.tensor(X_train_TWO, dtype=torch.float32).unsqueeze(1)
+    # y_train_tensor_TWO = torch.tensor(y_train_TWO, dtype=torch.long)
+    #
+    # X_test_tensor_TWO = torch.tensor(X_test_TWO, dtype=torch.float32).unsqueeze(1)
+    # y_test_tensor_TWO = torch.tensor(y_test_TWO, dtype=torch.long)
+
+    # return (X_train_tensor, y_train_tensor), (X_test_tensor, y_test_tensor), (X_train_tensor_TWO, y_train_tensor_TWO), (X_test_tensor_TWO, y_test_tensor_TWO), scaler
     return (X_train_tensor, y_train_tensor), (X_test_tensor, y_test_tensor), scaler
 
 if __name__ == '__main__':
-    create_dataset(symbol='EURUSDrfd', window_len=30, predict_len=3)
+    create_dataset(symbol='EURUSDrfd', window_len=30, predict_len=10)
--- models/Models.py
+++ models/Models.py
@@ -1,19 +1,39 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from torch import optim, Tensor
+import numpy as np
+from sklearn.utils import class_weight
+
 
 class TradingCNN(nn.Module):
-    def __init__(self, n_features=5, n_classes=2):
+    def __init__(self, n_features=4, n_classes=2):
+        """
+        Первый сверточный слой:
+
+        self.conv1 = ...: Мы создаем слой и сохраняем его как атрибут модели.
+        in_channels=1: На входе ожидается тензор с 1 каналом. В трейдинге это обычно "картинка", где высота — это время (свечи),
+            а ширина — признаки (цена, объем). Канал здесь один (черно-белое изображение).
+        out_channels=32: Слой создаст 32 новые карты признаков. Каждая карта будет "видеть" свой уникальный паттерн.
+        kernel_size=(3, n_features): Размер фильтра (ядра).
+        (3): Свертка по времени (берет 3 свечи).
+        (n_features): Свертка по всем признакам сразу. Это позволяет фильтру видеть взаимосвязь между ценой и объемом одновременно.
+        padding=(1, 0): Добавляет 1 единицу отступа по высоте (времени), чтобы сохранить длину временного ряда. По ширине отступ 0.
+
+        :param n_features: Числовой параметр, описывающий количество признаков (open, high, low, close)...
+        :param n_classes: числовой параметр, описывающий количество классов в классификации (buy, sell)
+        """
         super(TradingCNN, self).__init__()
 
         # Сверточные слои
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, n_features), padding=(1, 0))
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 1), padding=(1, 0))
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 1), padding=(1, 0))
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3, n_features), padding=(1, 0))
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 1), padding=(1, 0))
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 1), padding=(1, 0))
 
         # Полносвязные слои (для более сложного анализа)
-        self.fc1 = nn.Linear(128 * 10, 256)  # 10 — длина окна после pooling
-        self.fc2 = nn.Linear(256, 64)
+        self.fc1 = nn.Linear(768, 700)  # 10 — длина окна после pooling
+        self.fc2 = nn.Linear(700, 64)
 
         # Выходной слой
         self.output = nn.Linear(64, n_classes)  # Классификация
@@ -37,3 +57,50 @@ class TradingCNN(nn.Module):
 
         out = self.output(x)
         return out
+
+def train_model(model, train_loader, val_loader, target: Tensor, epochs=20):
+    unique = np.unique(target)
+    l = target.tolist()
+    weight = class_weight.compute_class_weight(class_weight='balanced', classes=unique, y=l)
+
+    criterion = nn.CrossEntropyLoss(weight=torch.tensor(weight, dtype=torch.float32))
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train_losses, val_accuracies = [], []
+
+    for epoch in range(epochs):
+        model.train()
+        epoch_loss = 0
+
+        for inputs, targets in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+
+        avg_loss = epoch_loss / len(train_loader)
+        train_losses.append(avg_loss)
+
+        # Валидация
+        model.eval()
+        correct, total = 0, 0
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += targets.size(0)
+                correct += (predicted == targets).sum().item()
+
+        accuracy = correct / total
+        val_accuracies.append(accuracy)
+
+        print(f'Epoch {epoch + 1}/{epochs} | Loss: {avg_loss:.4f} | Val Acc: {accuracy:.4f}')
+
+    plt.figure(figsize=(8, 4))
+    plt.plot(train_losses, label='Train Loss')
+    plt.plot(val_accuracies, label='Val Accuracy')
+    plt.legend()
+    plt.title('Обучение модели')
+    plt.show()
